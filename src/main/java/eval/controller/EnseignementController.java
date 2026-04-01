1e9390be16b7fbededb6c8cfe8e2eed327aef0ec
chakrialim <limchakriasaurphear@gmail.com>
2026-04-02 01:03:07 +0200

    essai docker

--- src/main/java/eval/controller/EnseignementController.java
+++ src/main/java/eval/controller/EnseignementController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
-@RequestMapping("/enseignements")
+@RequestMapping("/api/enseignements")
 public class EnseignementController {
 
     private final EnseignementRepository enseignementRepository;
